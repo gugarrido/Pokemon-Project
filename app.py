9464df7a43fcead97897641692a23d80bfb0044a
gustavogarrido <gustavogarrido@ibm.com>
2021-02-23 21:05:04 -0300

    added pokedex template page

--- app.py
+++ app.py
@@ -10,18 +10,23 @@ def index():
         pokemonname = request.form["pokename"]
         pokemonname = pokemonname.lower()
         if pokemonname:
-            pokemoninfo = pb.pokemon(pokemonname)
-            pokemonid = pokemoninfo.id
+            # pokemoninfo = pb.pokemon(pokemonname)
+            # pokemonid = pokemoninfo.id
             return redirect(url_for("poke", pokename=pokemonname, pokeid=pokemoninfo.id))
         else: 
             return f"<h1>ERROR: NAME CAN NOT BE EMPTY</h1>" 
     else:    
         return render_template("index.html")
 
-@app.route('/<pokename>/<pokeid>')
-def poke(pokename, pokeid):
-    pokeimage = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokeid}.png'
-    return f"<img src={pokeimage}><br> <h1>{pokename}</h1>"
+# @app.route('/<pokename>/<pokeid>')
+# def poke(pokename, pokeid):
+#     pokeimage = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokeid}.png'
+#     return f"<img src={pokeimage}><br> <h1>{pokename}</h1>"
+
+
+@app.route('/pokedex')
+def pokedex():
+    return render_template('pokedex.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
